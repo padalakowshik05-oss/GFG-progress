405dde9878b98d139997822eaba47180c7375762
padala kowshik <padalakowshik05@gmail.com>
2026-08-19 17:21:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor and Ceil in Unsorted/floor-and-ceil-in-unsorted.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getFloorAndCeil(int x, int[] arr) {
+        int floor = -1;
+        int ceil = -1;
+
+        for (int num : arr) {
+            if (num <= x) {
+                floor = Math.max(floor, num);
+            }
+            if (num >= x) {
+                if (ceil == -1) {
+                    ceil = num;
+                } else {
+                    ceil = Math.min(ceil, num);
+                }
+            }
+        }
+
+        return new int[]{floor, ceil};
+    }
+}
