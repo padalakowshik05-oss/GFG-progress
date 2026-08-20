a284382e69057d481b80d89e39fc360fdee1b047
padala kowshik <padalakowshik05@gmail.com>
2026-08-20 20:57:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Convert To Zig-Zag/convert-to-zig-zag.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static void zigZag(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 2 == 0) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i] < arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+        
+    }
+}
