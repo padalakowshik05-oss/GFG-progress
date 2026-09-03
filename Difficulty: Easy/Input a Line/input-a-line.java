6a5a4e806709b7f521e67712021b77671fb89f9b
padala kowshik <padalakowshik05@gmail.com>
2026-09-03 21:40:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Input a Line/input-a-line.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void getLine() {
+        String s = sc.nextLine();
+
+        System.out.println(s);
+    }
+}
