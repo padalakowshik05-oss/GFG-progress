0a73edf66ac3d52941d46e55b4c8e1a082aab120
padala kowshik <padalakowshik05@gmail.com>
2026-09-06 15:15:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove All Except Alphabets/remove-all-except-alphabets.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeChars(String s) {
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')){
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+        
+    }
+};
