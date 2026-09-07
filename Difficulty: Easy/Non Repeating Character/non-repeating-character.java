531b33a240812f3e885d240b6055e340568e4efb
padala kowshik <padalakowshik05@gmail.com>
2026-09-07 22:07:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c: s.toCharArray()){
+            if(map.get(c)==1){
+                return c;
+            }
+        }
+        return '$';
+        
+    }
+}
